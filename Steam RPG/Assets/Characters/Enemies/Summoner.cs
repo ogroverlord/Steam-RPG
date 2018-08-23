27fc3903e2a32e50dcb6c5ba4ea1e1f2d76ae0d4
ogroverlord <karol_michalczyk@interia.pl>
2018-08-23 22:06:42 +0200

    Reorganization of the Assets Folders
    Folders were moved and renamed.

---
+++ Steam RPG/Assets/Characters/Enemies/Summoner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Summoner : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Steam RPG/Assets/Weapons/Projectiles/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float projectileSpeed = 10f;
+    float damage;
+
+    public void SetDamage(float damage)
+    {
+        this.damage = damage;
+    }
+
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        IDamageable damagableComponent = collision.gameObject.GetComponent<IDamageable>();
+            
+        if(damagableComponent != null)
+        {
+            damagableComponent.TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+
+    
+
+
+}
